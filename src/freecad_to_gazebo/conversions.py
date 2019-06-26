26e6c96493f6a658af246c79844628c81f2b70d9
Dawit Abate <dawitabate2@gmail.com>
2019-06-26 13:45:46 +0300

    Added conversion helpers

---
+++ src/freecad_to_gazebo/conversions.py
@@ -0,0 +1,12 @@
+import FreeCAD
+from math import radians as _radians
+
+
+def deg2rad(d):
+    '''Converts degrees to radians'''
+    return _radians(d)
+
+def flt2str(f):
+    '''Converts floats to formatted string'''
+    return '{:.6f}'.format(f)
+
